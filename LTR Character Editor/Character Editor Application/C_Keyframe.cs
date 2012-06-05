fa44047036d6d10a73e669b2cfa14b0a5c51c138
Lee <lee.j.cephas@gmail.com>
2012-06-05 02:49:31 -0500

    refactoring - keyframe class looks good
    animation class next

---
+++ LTR Character Editor/Character Editor Application/C_Keyframe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace CharacterEditor
+{
+    class C_Keyframe
+    {
+        private C_Bone[] m_boneArray;
+
+
+        public C_Keyframe()
+        {
+            m_boneArray = new C_Bone[12];// 12 is current maximum bone count!!
+        }
+    }
+}
