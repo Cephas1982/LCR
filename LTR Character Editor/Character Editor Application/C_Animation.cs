2cb032e6ba39a8b4eee7abab8b62e6690cdd03b4
Lee Cephas <Cephas1982@yahoo.com>
2012-06-06 10:29:34 -0500

    Refactor - C_Animation and C_Keyframe added
    C_Skeleton could be changed to C_Entity?
    and C_Entity would be a base class for player objects

---
+++ LTR Character Editor/Character Editor Application/C_Animation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CharacterEditor
+{
+    class C_Animation
+    {
+        //each animation class will have a list of keyframes, 
+        //and each keyframe contains 1 array of bones
+        private List<C_Keyframe> l_keyfame { get { return l_keyfame; } set { l_keyfame = value; } }
+        private String m_name;
+
+
+        public C_Animation()
+        {
+            l_keyfame = new List<C_Keyframe>();
+        }
+
+        public void Load(string animationName)
+        {
+            //this will load all keyframes that belong to the requested animation
+        }        
+
+
+    }
+
+
+}
